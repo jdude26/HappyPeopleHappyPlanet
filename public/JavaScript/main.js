005790ddddc619930be8789ddf4e055616c68b28
jdude26 <ffilo26@aol.com>
2017-05-24 15:31:29 -0700

    happyDesign

--- public/JavaScript/main.js
+++ public/JavaScript/main.js
@@ -1,4 +1,58 @@
+	$(window).on( "load" ,function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
+
 document.addEventListener('DOMContentLoaded', function(){
 	"use strict";
 	var trigger = new ScrollTrigger();
 });
+
+mdc.textfield.MDCTextfield.attachTo(document.querySelector('.mdc-textfield'));
+
+mdc.autoInit();
+
+var buttons = document.querySelectorAll('.mdc-button, .mdc-fab');
+for (var i = 0, button; button = buttons[i]; i++) {
+  mdc.ripple.MDCRipple.attachTo(button);
+}
+
+var nodes = document.querySelectorAll('.mdc-icon-toggle');
+for (var i = 0, node; node = nodes[i]; i++) {
+  mdc.iconToggle.MDCIconToggle.attachTo(node);
+}
+
+var checkboxes = document.querySelectorAll('.mdc-checkbox');
+for (var i = 0, checkbox; checkbox = checkboxes[i]; i++) {
+  new mdc.checkbox.MDCCheckbox(checkbox);
+}
+
+var radios = document.querySelectorAll('.mdc-radio');
+for (var i = 0, radio; radio = radios[i]; i++) {
+  new mdc.radio.MDCRadio(radio);
+}
+
+var interactiveListItems = document.querySelectorAll('.mdc-list-item');
+for (var i = 0, li; li = interactiveListItems[i]; i++) {
+  mdc.ripple.MDCRipple.attachTo(li);
+  // Prevent link clicks from jumping demo to the top of the page
+  li.addEventListener('click', function(evt) {
+    evt.preventDefault();
+  });
+}
+mdc.dialog.MDCDialog.attachTo(document.querySelector('#my-mdc-dialog'));
+var dialog = new mdc.dialog.MDCDialog(document.querySelector('#my-mdc-dialog'));
+
+dialog.listen('MDCDialog:accept', function() {
+  console.log('accepted');
+})
+
+dialog.listen('MDCDialog:cancel', function() {
+  console.log('canceled');
+})
+
+document.querySelector('#privacy').addEventListener('click', function (evt) {
+  dialog.lastFocusedTarget = evt.target;
+  dialog.show();
+})
+
